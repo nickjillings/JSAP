2586ca3ec338bd731e0d0359f269e0bdbf0671f8
Nicholas Jillings <nicholas.jillings@mail.bcu.ac.uk>
2016-08-11 10:51:26 +0100

    Squashed 'js-xtract/' changes from faae09c..fa514e8
    fa514e8 Readme
    f912da0 AudioBuffer.xtract_process_frame. Returned should be object not string
    649af83 Improved callback result
    d67442e Minor
    59e0389 Minor
    8670007 Reworked OO views
    44d30fb Minor
    4657413 Minor
    f960aa7 Minor
    2b31988 Minor
    0180bea Minor
    c22d360 Minor
    c9e1333 Minor
    7cba754 Merge branch 'Develop'
    df02fd3 Web Audio section uses Object Oriented calls
    d4f19e0 Typo
    6f5725f Updated Readme.md
    5540258 Updated Readme.md
    81d9d1a Updated Readme.md
    734da1e Created toJSON for Objects. New constructor for TimeData from Float32/64Array or TimeData
    64bf9bf Wrapped AnalyserNode and AudioBuffer inside If statements for non WAA.
    a2b0344 Merge branch 'Develop'
    b9d88a5 Added new all-in-one jsXtract.min.js. Added more features to TimeData. Cleaned up jsXtract superobject
    9aece3d Updated Objects to autocalculate and store intermediary values
    4314d24 Moved FB stuff to develop.
    2557fe3 Minor
    144a275 Minor + Beautifying
    9523899 Added crest and spectral crest factor to OO
    c3bed26 Beautify js-xtract.js. Added Object-Oriented hierachy
    d36671f Started OO parts
    912715b Updating pitch_FB and resample
    08fbb55 Added chroma toolbox filterbanks
    56da497 Added xtract_resample. Implementing chroma
    e00f839 Use Float64Array on all defined arrays
    1737eb4 Implemented LPC
    8b42936 Completed jsXtract() object
    018b1e2 Minor
    2ff8573 Minor
    7557d6e Minor
    a31b50d Minor
    8e1ef91 Minor
    161a2c3 New Feature object for jsXtract
    c57f2ce Updated Wavelet F0 to include history. Trimmed JSXtract Object
    
    git-subtree-dir: js-xtract
    git-subtree-split: fa514e8dd150e2921c5ae02e2d3939aa84aa95a2

--- jsXtract-wa.js
+++ jsXtract-wa.js
@@ -23,108 +23,127 @@
 
 // This binds the js-xtract with the Web Audio API AudioBuffer and AnalyserNodes
 
-AnalyserNode.prototype.getXtractData = function () {
-    var N = this.fftSize / 2;
-    var data = {
-        "window_size": N,
-        "sample_rate": this.context.sampleRate,
-        "TimeData": new Float32Array(N),
-        "SpectrumData": new Float32Array(2 * N + 2)
-    };
-    var SpectrumLogData = new Float32Array(N + 1);
-    this.getFloatFrequencyData(SpectrumLogData);
-    if (this.getFloatTimeDomainData == undefined) {
-        var TempTime = new Uint8Array(data.window_size);
-        this.getByteTimeDomainData(TempTime);
-        for (var n = 0; n < data.window_size; n++) {
-            var num = TempTime[n];
-            num /= 128.0;
-            num -= 1.0;
-            data.TimeData[n] = num;
+if (AnalyserNode) {
+
+    AnalyserNode.prototype.timeData = undefined;
+    AnalyserNode.prototype.spectrumData = undefined;
+    AnalyserNode.prototype.getXtractData = function () {
+        if (this.timeData == undefined || this.scpectrumData == undefined) {
+            this.timeData = new TimeData(this.fftSize, this.context.sampleRate);
+            this.spectrumData = new SpectrumData(this.frequencyBinCount, this.context.sampleRate);
         }
-        delete TempTime;
-    } else {
-        this.getFloatTimeDomainData(data.TimeData);
-    }
-    for (var N = SpectrumLogData.length, i = 0; i < N; i++) {
-        data.SpectrumData[i + N] = i * ((data.sample_rate / 2) / N);
-        data.SpectrumData[i] = Math.pow(10, SpectrumLogData[i] / 20);
-    }
-    return data;
-}
-
-AnalyserNode.prototype.frameCallback = function (func, arg_this) {
-    // Perform a callback on each frame
-    if (this.callbackObject == undefined) {
-        this.callbackObject = this.context.createScriptProcessor(this.fftSize, 1, 1);
-        this.connect(this.callbackObject);
-    }
-    var _func = func;
-    var _arg_this = arg_this;
-    var self = this;
-    this.callbackObject.onaudioprocess = function (e) {
-        var result = _func.call(_arg_this, self.getXtractData());
-        var N = e.outputBuffer.length;
-        var output = new Float32Array(N);
-        if (typeof result != "number") {
-            result = 0.0;
+        var dst = new Float32Array(this.fftSize);
+        if (this.getFloatTimeDomainData) {
+            this.getFloatTimeDomainData(dst);
+        } else {
+            var view = new UInt8Array(this.fftSize);
+            this.getByteTimeDomainData(view);
+            for (var i=0; i<this.fftSize; i++) {
+                dst[i] = view[i];
+                dst[i] = (dst[i]/127.5)-1;
+            }
         }
-        output.fill(result);
-        e.outputBuffer.copyToChannel(output,0,0);
-    }
-}
-
-AnalyserNode.prototype.clearCallback = function () {
-    this.disconnect(this.callbackObject);
-    this.callbackObject = undefined;
-}
-
-AnalyserNode.prototype.xtractFrame = function (func, arg_this) {
-    // Collect the current frame of data and perform the callback function
-    func.call(arg_this, this.getXtractData());
-}
-
-AudioBuffer.prototype.xtract_get_data_frames = function (frame_size, hop_size) {
-    if (typeof frame_size != "number") {
-        throw ("xtract_get_data_frames requires the frame_size to be defined");
-    }
-    if (frame_size <= 0 || frame_size != Math.floor(frame_size)) {
-        throw ("xtract_get_data_frames requires the frame_size to be a positive integer");
-    }
-    if (hop_size == undefined) {
-        hop_size = frame_size;
-    }
-    if (hop_size <= 0 || hop_size != Math.floor(hop_size)) {
-        throw ("xtract_get_data_frames requires the hop_size to be a positive integer");
-    }
-    var frames = [this.numberOfChannels];
-    var N = this.length;
-    var K = Math.ceil(N / frame_size);
-    for (var c = 0; c < this.numberOfChannels; c++) {
-        var data = this.getChannelData(c);
-        frames[c] = data.xtract_get_data_frames(frame_size, hop_size, true);
-    }
-    return frames;
-}
-
-AudioBuffer.prototype.xtract_process_frame_data = function (func, frame_size, hop_size, arg_this) {
-    // Process each data point and return a JSON of each frame result from func
-    // Func must return something for this to be a useful feature
-    // func has three arguments (currentFrame, previousFrame, previousResult);
-    var result = {
-        num_channels: this.numberOfChannels,
-        channel_results: []
-    };
-    var K = frame_size >> 1;
-    var frame_time = 0;
-    for (var c = 0; c < this.numberOfChannels; c++) {
-        var channel_vector = this.getChannelData(c);
-        var channel_result = {
-            num_frames: channel_vector.length,
-            results: []
+        this.timeData.copyDataFrom(dst);
+        this.timeData.result.spectrum = this.spectrumData;
+        var LogStore = new Float32Array(this.frequencyBinCount);
+        this.getFloatFrequencyData(LogStore);
+        for (var i=0; i<this.frequencyBinCount; i++) {
+            LogStore[i] = Math.pow(10.0, LogStore[i]/20);
         }
-        result.channel_results.push(channel_vector.xtract_process_frame_data(func, this.sampleRate, frame_size, hop_size, arg_this));
-
+        this.spectrumData.copyDataFrom(LogStore);
+        return this.timeData;
     }
-    return result;
+    AnalyserNode.prototype.previousFrame = undefined;
+    AnalyserNode.prototype.previousResult = undefined;
+    AnalyserNode.prototype.frameCallback = function (func, arg_this) {
+        // Perform a callback on each frame
+        // The function callback has the arguments (current_frame, previous_frame, previous_result)
+        if (this.callbackObject == undefined) {
+            this.callbackObject = this.context.createScriptProcessor(this.fftSize, 1, 1);
+            this.connect(this.callbackObject);
+        }
+        var _func = func;
+        var _arg_this = arg_this;
+        var self = this;
+        this.callbackObject.onaudioprocess = function (e) {
+            var current_frame = self.getXtractData();
+            this.previousResult = _func.call(_arg_this,current_frame, this.previousFrame, this.previousResult);
+            this.previousFrame = current_frame;
+            var N = e.outputBuffer.length;
+            var output = new Float32Array(N);
+            var result = this.previousResult;
+            if (typeof this.previousResult != "number") {
+                result = 0.0;
+            }
+            for (var i=0; i<N; i++) {
+                output[i] = result;
+            }
+            e.outputBuffer.copyToChannel(output, 0, 0);
+        }
+    }
+
+    AnalyserNode.prototype.clearCallback = function () {
+        this.disconnect(this.callbackObject);
+        this.callbackObject = undefined;
+    }
+
+    AnalyserNode.prototype.xtractFrame = function (func, arg_this) {
+        // Collect the current frame of data and perform the callback function
+        func.call(arg_this, this.getXtractData());
+    }
+}
+
+if (AudioBuffer) {
+
+    AudioBuffer.prototype.xtract_get_data_frames = function (frame_size, hop_size) {
+        if (typeof frame_size != "number") {
+            throw ("xtract_get_data_frames requires the frame_size to be defined");
+        }
+        if (frame_size <= 0 || frame_size != Math.floor(frame_size)) {
+            throw ("xtract_get_data_frames requires the frame_size to be a positive integer");
+        }
+        if (hop_size == undefined) {
+            hop_size = frame_size;
+        }
+        if (hop_size <= 0 || hop_size != Math.floor(hop_size)) {
+            throw ("xtract_get_data_frames requires the hop_size to be a positive integer");
+        }
+        var frames = [];
+        var N = this.length;
+        var K = Math.ceil(N / frame_size);
+        for (var c = 0; c < this.numberOfChannels; c++) {
+            var data = this.getChannelData(c);
+            frames[c] = [];
+            for (var k=0; k<K; k++) {
+                var frame = new TimeData(hop_size, this.sampleRate);
+                frame.copyDataFrom(data.subarray(frame_size*k, frame_size*k+hop_size));
+                frames[c].push(frame);
+            }
+        }
+        return frames;
+    }
+
+    AudioBuffer.prototype.xtract_process_frame_data = function (func, frame_size, hop_size, arg_this) {
+        // Process each data point and return a JSON of each frame result from func
+        // Func must return something for this to be a useful feature
+        // func has the following arguments (element, index, array);
+        var result = {
+            num_channels: this.numberOfChannels,
+            channel_results: []
+        };
+        var K = frame_size >> 1;
+        var frames = this.xtract_get_data_frames(frame_size,hop_size);
+        for (var c = 0; c < frames.length; c++) {
+            result.channel_results[c] = {
+                num_frames: frames[c].length,
+                results: []
+            };
+            for (var k=0; k<frames[c].length; k++) {
+                var frame = frames[c][k];
+                func.call(arg_this,frame,k,frames[c]);
+                result.channel_results[c].results[k] = JSON.parse(frame.toJSON());
+            }
+        }
+        return result;
+    }
 }
