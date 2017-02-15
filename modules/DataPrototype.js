b2d4e23079b4e817dae4ede235153143572c7b1b
Nicholas Jillings <nicholas.jillings@mail.bcu.ac.uk>
2017-02-15 11:50:46 +0000

    Squashed 'js-xtract/' changes from a656c44..ea42e4e
    ea42e4e Updated readme.md
    f8cea65 CodeClimate: Remove similar code checks
    ca2e25c Simplified zeroDataRange
    f904331 Finished DataProto Superclass
    8c76f15 Fold remaining defineProperties
    7f929d2 SpectrumData: Fold all defineProperty into defineProperties
    c1f0688 Fix build issues
    c1a2c5a TimeData: Fold all defineProperty into defineProperties
    ffaa54d Fix silly type issue
    eb3c750 Remove polyfill for String.endswith
    65fbfff Correctly indent yml
    f2722ac Reduced duplicate code in DataPrototype
    b7f38f3 eslint convention
    f3374d4 Re-ordered layout. Created singleton jsXtract for DataProto.
    2266caf Remove illegal Global Prototype manipulations
    581d6f7 Reduce duplicated code
    23b1b19 More ESLint. Return results and type checkers
    715d1a0 Remove heavily duplicated code for Float32/64 Prototypes
    5b6897a Fix Superclasses
    7ed14cf Some chaining rubbish
    69c3d3c More tidying up for JSHint
    1c76d50 Fixed xtract_array_interlace
    0106ffa Merge branch 'master' of https://github.com/nickjillings/js-xtract
    77b204b Tidying up for JSHint
    f9b7e22 Add badges
    aaaf8c6 Add Travis CI and CodeClimate
    5407c23 Tidy up repo
    
    git-subtree-dir: js-xtract
    git-subtree-split: ea42e4e4b7b1835596472a66f0d9d1648c098341

--- modules/DataPrototype.js
+++ modules/DataPrototype.js
@@ -1,38 +1,170 @@
-var jsXtract = new function () {
+/*globals Float32Array, Float64Array */
+/*globals xtract_init_dct, xtract_init_mfcc, xtract_init_bark */
 
-    var DataProto = function (parent) {
-        var _result = {};
-        //this.__proto__ = parent;
-        this.clearResult = function () {
-            _result = {};
+// Create the Singleton
+var jsXtract = (function () {
+
+    function searchMapProperties(map, properties) {
+        var match = map.find(function (e) {
+            for (var prop in properties) {
+                if (e[prop] !== properties[prop]) {
+                    return false;
+                }
+            }
+            return true;
+        });
+        return match;
+    }
+
+    var dct_map = {
+        parent: this,
+        store: [],
+        createCoefficients: function (N) {
+            var match = searchMapProperties(this.store, {
+                N: N
+            });
+            if (!match) {
+                match = {
+                    N: N,
+                    data: xtract_init_dct(N)
+                };
+                this.store.push(match);
+            }
+            return match.data;
         }
+    };
 
-        Object.defineProperty(this, "result", {
+    var mfcc_map = {
+        parent: this,
+        store: [],
+        createCoefficients: function (N, nyquist, style, freq_min, freq_max, freq_bands) {
+            var search = {
+                N: N,
+                nyquist: nyquist,
+                style: style,
+                freq_min: freq_min,
+                freq_max: freq_max,
+                freq_bands: freq_bands
+            };
+            var match = searchMapProperties(this.store, search);
+            if (!match) {
+                match = search;
+                match.data = xtract_init_mfcc(N, nyquist, style, freq_min, freq_max, freq_bands);
+                this.store.push(match);
+            }
+            return match.data;
+        }
+    };
+
+    var bark_map = {
+        parent: this,
+        store: [],
+        createCoefficients: function (N, sampleRate, numBands) {
+            var search = {
+                N: N,
+                sampleRate: sampleRate,
+                numBands: numBands
+            };
+            var match = searchMapProperties(this.store, search);
+            if (!match) {
+                match = search;
+                match.data = xtract_init_bark(N, sampleRate, numBands);
+                this.store.push(match);
+            }
+            return match.data;
+        }
+    };
+    var pub_obj = {};
+    Object.defineProperties(pub_obj, {
+        "createDctCoefficients": {
+            "value": function (N) {
+                return dct_map.createCoefficients(N);
+            }
+        },
+        "createMfccCoefficients": {
+            "value": function (N, nyquist, style, freq_min, freq_max, freq_bands) {
+                return mfcc_map.createCoefficients(N, nyquist, style, freq_min, freq_max, freq_bands);
+            }
+        },
+        "createBarkCoefficients": {
+            "value": function (N, sampleRate, numBands) {
+                if (typeof numBands !== "number" || numBands < 0 || numBands > 26) {
+                    numBands = 26;
+                }
+                return bark_map.createCoefficients(N, sampleRate, numBands);
+            }
+        }
+    });
+    return pub_obj;
+})();
+var DataProto = function (N, sampleRate) {
+    var _result = {},
+        _data = new Float64Array(N);
+    this.clearResult = function () {
+        _result = {};
+    };
+
+    Object.defineProperties(this, {
+        "result": {
             'get': function () {
                 return _result;
             },
             'set': function () {}
-        });
-
-        Object.defineProperty(this, "toJSON", {
+        },
+        "data": {
+            'value': _data
+        },
+        "getData": {
             'value': function () {
-                return this.__proto__.toJSON(_result);
+                return _data;
             }
-        });
-    };
-    DataProto.prototype = this;
-    DataProto.prototype.constructor = DataProto;
+        }
+    });
 
-    this.toJSON = function (result) {
+    this.zeroDataRange = function (start, end) {
+        if (_data.fill) {
+            _data.fill(0, start, end);
+        } else {
+            for (var n = start; n < end; n++) {
+                _data[n] = 0;
+            }
+        }
+        this.clearResult();
+    };
+
+    this.copyDataFrom = function (src, N, offset) {
+        if (typeof src !== "object" || src.length === undefined) {
+            throw ("copyDataFrom requires src to be an Array or TypedArray");
+        }
+        if (offset === undefined) {
+            offset = 0;
+        }
+        if (N === undefined) {
+            N = Math.min(src.length, _data.length);
+        }
+        N = Math.min(N + offset, _data.length);
+        for (var n = 0; n < N; n++) {
+            _data[n + offset] = src[n];
+        }
+        this.clearResult();
+    };
+
+    this.duplicate = function () {
+        var copy = this.prototype.constructor(N, sampleRate);
+        copy.copyDataFrom(_data);
+    };
+
+    this.toJSON = function () {
         var json = '{';
-        for (var property in result) {
-            if (!json.endsWith('{') && !json.endsWith(',')) {
+        for (var property in _result) {
+            var lastchar = json[json.length - 1];
+            if (lastchar !== '{' && lastchar !== ',') {
                 json = json + ', ';
             }
-            if (typeof result[property] == "number" && isFinite(result[property])) {
-                json = json + '"' + property + '": ' + result[property];
-            } else if (typeof result[property] == "object") {
-                switch (result[property].constructor) {
+            if (typeof _result[property] === "number" && isFinite(_result[property])) {
+                json = json + '"' + property + '": ' + _result[property];
+            } else if (typeof _result[property] === "object") {
+                switch (_result[property].constructor) {
                     case Array:
                     case Float32Array:
                     case Float64Array:
@@ -41,18 +173,18 @@ var jsXtract = new function () {
                     case PeakSpectrumData:
                     case HarmonicSpectrumData:
                         // Array
-                        json = json + '"' + property + '": ' + result[property].toJSON(result[property]);
+                        json = json + '"' + property + '": ' + _result[property].toJSON(_result[property]);
                         break;
                     default:
-                        json = json + '"' + property + '": ' + this.toJSON(result[property]);
+                        json = json + '"' + property + '": ' + this.toJSON(_result[property]);
                         break;
                 }
             } else {
-                json = json + '"' + property + '": "' + result[property].toString() + '"';
+                json = json + '"' + property + '": "' + _result[property].toString() + '"';
             }
         }
         return json + '}';
-    }
+    };
 
     function recursiveDelta(a, b) {
         //a and b are objects of Time/Spectrum/PeakS/HarmonicS Data
@@ -60,14 +192,14 @@ var jsXtract = new function () {
         var param, delta = {};
         for (param in a) {
             if (b[param]) {
-                if (typeof a[param] == "number") {
+                if (typeof a[param] === "number") {
                     delta[param] = a[param] - b[param];
                 } else {
                     switch (a[param].constructor) {
                         case Array:
                         case Float32Array:
                         case Float64Array:
-                            if (a[param].length == b[param].length) {
+                            if (a[param].length === b[param].length) {
                                 delta[param] = new Float64Array(a[param].length);
                             } else {
                                 delta[param] = [];
@@ -96,7 +228,7 @@ var jsXtract = new function () {
     this.computeDelta = function (compare) {
         this.result.delta = recursiveDelta(this.result, compare.result);
         return this.result.delta;
-    }
+    };
 
     this.computeDeltaDelta = function (compare) {
         if (!compare.result.delta || !this.result.delta) {
@@ -104,106 +236,22 @@ var jsXtract = new function () {
         }
         this.result.delta.delta = recursiveDelta(this.result.delta, compare.result.delta);
         return this.result.delta.delta;
-    }
-
-    var dct_map = {
-        parent: this,
-        store: [],
-        createCoefficients: function (N) {
-            var match = this.store.find(function (element) {
-                if (element.N == this) {
-                    return true;
-                }
-                return false;
-            }, N);
-            if (!match) {
-                match = {
-                    N: N,
-                    data: xtract_init_dct(N)
-                }
-                this.store.push(match);
-            }
-            return match.data;
-        }
-    }
-
-    var mfcc_map = {
-        parent: this,
-        store: [],
-        createCoefficients: function (N, nyquist, style, freq_min, freq_max, freq_bands) {
-            var search = {
-                N: N,
-                nyquist: nyquist,
-                style: style,
-                freq_min: freq_min,
-                freq_max: freq_max,
-                freq_bands: freq_bands
-            }
-            var match = this.store.find(function (element) {
-                for (var prop in this) {
-                    if (element[prop] != this[prop]) {
-                        return false;
-                    }
-                }
-                return true;
-            }, search);
-            if (!match) {
-                match = search;
-                match.data = xtract_init_mfcc(N, nyquist, style, freq_min, freq_max, freq_bands);
-                this.store.push(match);
-            }
-            return match.data;
-        }
-    }
-
-    var bark_map = {
-        parent: this,
-        store: [],
-        createCoefficients: function (N, sampleRate, numBands) {
-            var search = {
-                N: N,
-                sampleRate: sampleRate,
-                numBands: numBands
-            }
-            var match = this.store.find(function (element) {
-                for (var prop in element) {
-                    if (element[prop] != this[prop]) {
-                        return false;
-                    }
-                }
-                return true;
-            }, search);
-            if (!match) {
-                match = search;
-                match.data = xtract_init_bark(N, sampleRate, numBands);
-                this.store.push(match);
-            }
-            return match.data;
-        }
-    }
-
-    this.createDctCoefficients = function (N) {
-        return dct_map.createCoefficients(N);
-    }
-
-    this.createMfccCoefficients = function (N, nyquist, style, freq_min, freq_max, freq_bands) {
-        return mfcc_map.createCoefficients(N, nyquist, style, freq_min, freq_max, freq_bands);
-    }
-
-    this.createBarkCoefficients = function (N, sampleRate, numBands) {
-        if (typeof numBands != "number" || numBands < 0 || numBands > 26) {
-            numBands = 26;
-        }
-        return bark_map.createCoefficients(N, sampleRate, numBands);
-    }
-
-    this.createTimeDataProto = function () {
-        var node = new DataProto();
-        return node;
-    }
-
-    this.createSpectrumDataProto = function () {
-        var node = new DataProto();
-        return node;
-    }
-}
+    };
+};
+DataProto.prototype.createDctCoefficients = function (N) {
+    return jsXtract.createDctCoefficients(Number(N));
+};
+DataProto.prototype.createMfccCoefficients = function (N, nyquist, style, freq_min, freq_max, freq_bands) {
+    N = Number(N);
+    nyquist = Number(nyquist);
+    freq_min = Number(freq_min);
+    freq_max = Number(freq_max);
+    freq_bands = Number(freq_bands);
+    return jsXtract.createMfccCoefficients(N, nyquist, style, freq_min, freq_max, freq_bands);
+};
+DataProto.prototype.createBarkCoefficients = function (N, sampleRate, numBands) {
+    N = Number(N);
+    sampleRate = Number(sampleRate);
+    numBands = Number(numBands);
+    return jsXtract.createBarkCoefficients(N, sampleRate, numBands);
+};
--- modules/HarmonicSpectrumData.js
+++ modules/HarmonicSpectrumData.js
@@ -1,42 +1,44 @@
+/*globals Float32Array, Float64Array */
+/*globals window, console */
 var HarmonicSpectrumData = function (N, sampleRate, parent) {
-    if (N == undefined || N <= 0) {
+    if (N === undefined || N <= 0) {
         console.error("SpectrumData constructor requires N to be a defined, whole number");
         return;
     }
-    if (sampleRate == undefined) {
+    if (sampleRate === undefined) {
         sampleRate = Math.PI;
     }
-    this.__proto__ = new PeakSpectrumData(N);
-    this.__proto__.constructor = HarmonicSpectrumData;
+    PeakSpectrumData.call(this, N);
 
-    Object.defineProperty(this, "features", {
-        'get': function () {
-            return this.constructor.prototype.features;
+    Object.defineProperties(this, {
+        "features": {
+            'get': function () {
+                return this.constructor.prototype.features;
+            },
+            'set': function () {}
         },
-        'set': function () {}
-    });
-
-    // Harmonic Spectrum features
-    Object.defineProperty(this, "odd_even_ratio", {
-        'value': function () {
-            if (this.result.odd_even_ratio == undefined) {
-                if (this.f0 == undefined) {
-                    this.spectral_fundamental(this.getData(), this.sampleRate);
+        "odd_even_ratio": {
+            'value': function () {
+                if (this.result.odd_even_ratio === undefined) {
+                    if (this.f0 === undefined) {
+                        this.spectral_fundamental(this.data(), this.sampleRate);
+                    }
+                    this.result.odd_even_ratio = xtract_odd_even_ratio(this.data(), this.f0);
                 }
-                this.result.odd_even_ratio = xtract_odd_even_ratio(this.getData(), this.f0);
+                return this.result.odd_even_ratio;
             }
-            return this.result.odd_even_ratio;
+        },
+        "noisiness": {
+            'value': function () {
+                if (parent.constructor !== PeakSpectrumData) {
+                    this.result.noisiness = null;
+                } else {
+                    this.result.noisiness = xtract_noisiness(this.nonzero_count(), parent.nonzero_count());
+                }
+                return this.result.noisiness;
+            }
         }
     });
-
-    Object.defineProperty(this, "noisiness", {
-        'value': function () {
-            if (parent.constructor !== PeakSpectrumData) {
-                this.result.noisiness = null;
-            } else {
-                this.result.noisiness = xtract_noisiness(this.nonzero_count(), parent.nonzero_count());
-            }
-            return this.result.noisiness;
-        }
-    });
-}
+};
+HarmonicSpectrumData.prototype = Object.create(PeakSpectrumData.prototype);
+HarmonicSpectrumData.prototype.constructor = HarmonicSpectrumData;
